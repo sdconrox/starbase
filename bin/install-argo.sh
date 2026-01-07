d6c25c6e10ea17bf7b230205782bc5c400ae9656
Shane Conroy <sdconrox@gmail.com>
2026-01-06 22:29:40 -0500

    Added app configs. Fixing path for starbase-apps.

---
+++ bin/install-argo.sh
@@ -0,0 +1,90 @@
+kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
+
+cat > /tmp/argocd-min.yaml <<'YAML'
+# Keep this minimal; we can tune later
+
+# Install CRDs with the chart (recommended for Helm installs)
+crds:
+  install: true
+YAML
+
+helm upgrade --install argocd argo/argo-cd \
+  -n argocd \
+  -f gitops/clusters/starbase/argocd/install/values.yaml \
+  --wait
+
+# Release "argocd" does not exist. Installing it now.
+# NAME: argocd
+# LAST DEPLOYED: Tue Jan  6 22:12:38 2026
+# NAMESPACE: argocd
+# STATUS: deployed
+# REVISION: 1
+# DESCRIPTION: Install complete
+# TEST SUITE: None
+# NOTES:
+# In order to access the server UI you have the following options:
+
+# 1. kubectl port-forward service/argocd-server -n argocd 8080:443
+
+#     and then open the browser on http://localhost:8080 and accept the certificate
+
+# 2. enable ingress in the values file `server.ingress.enabled` and either
+#       - Add the annotation for ssl passthrough: https://argo-cd.readthedocs.io/en/stable/operator-manual/ingress/#option-1-ssl-passthrough
+#       - Set the `configs.params."server.insecure"` in the values file and terminate SSL at your ingress: https://argo-cd.readthedocs.io/en/stable/operator-manual/ingress/#option-2-multiple-ingress-objects-and-hosts
+
+
+# After reaching the UI the first time you can login with username: admin and the random password generated during the installation. You can find the password by running:
+
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+# (You should delete the initial secret afterwards as suggested by the Getting Started Guide: https://argo-cd.readthedocs.io/en/stable/getting_started/#4-login-using-the-cli)
+
+cat > gitops/clusters/starbase/argocd/install/bootstrap-applications.yaml <<'YAML'
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: starbase-apps
+  namespace: argocd
+  annotations:
+    argocd.argoproj.io/sync-wave: "-10"
+spec:
+  project: default
+  source:
+    repoURL: REPLACE_ME_REPO_URL
+    targetRevision: HEAD
+    path: gitops/clusters/starbase/apps
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: argocd
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true
+---
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: starbase-platform
+  namespace: argocd
+  annotations:
+    argocd.argoproj.io/sync-wave: "0"
+spec:
+  project: default
+  source:
+    repoURL: REPLACE_ME_REPO_URL
+    targetRevision: HEAD
+    path: gitops/clusters/starbase/platform
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: argocd
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true
+YAML
+
+kubectl apply -f gitops/clusters/starbase/argocd/install/bootstrap-applications.yaml
